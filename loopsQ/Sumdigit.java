699df5bf36c966b1879a213099b8462a91706694
Vishal Tyagi <147123885+insanetyagi@users.noreply.github.com>
2023-10-18 16:18:08 +0530

    Add files via upload

---
+++ loopsQ/Sumdigit.java
@@ -0,0 +1,35 @@
+package LoppQ;
+
+
+    import java.util.Scanner;
+
+public class Sumdigit {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        System.out.print("Enter the number ");
+        int num = x.nextInt();
+
+        int sum = calculateDigitSum(num);
+
+        System.out.println("Sum of digits in " + num + " is: " + sum);
+
+        x.close();
+    }
+
+    public static int calculateDigitSum(int num) {
+        num = Math.abs(num);
+
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit;
+
+            num /= 10;
+        }
+
+        return sum;
+    }
+}
+
